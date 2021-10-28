29031766de67d31c9d2e030e2755609e15d2950c
udayuk7 <93322281+udayuk7@users.noreply.github.com>
2021-10-28 16:33:01 +0530

    Add files via upload

---
+++ sum of n even numers.py
@@ -0,0 +1,7 @@
+n=int(input("ENTER A VALUE"))
+s=n*(n+1)
+print("sum of first",n,"even natural numbers is:",s)
+
+n=int(input("ENTER A VALUE"))
+s=n*(n+1)
+print("sum of first",n,"even natural numbers is:"
---
+++ sum of odd numbers.py
@@ -0,0 +1,10 @@
+n=int(input("enter a value"))
+
+s=n*(n+1)//2
+print("sum of first",n,"natural numbers is:",s)
+
+s=n**2
+print("sum of first",n,"odd natural numbers is:",s)
+
+s=n*(n+1)
+print("sum of first",n,"even natural numbers is:",s)
