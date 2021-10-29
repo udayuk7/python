ec573c353f9914c165977d7dc5784e1f9c20e7d8
udayuk7 <93322281+udayuk7@users.noreply.github.com>
2021-10-29 16:38:20 +0530

    Add files via upload

---
+++ logical operators-and.py
@@ -0,0 +1,9 @@
+age=int(input('enter your age:'))
+if age<=12 :
+    print('child')
+elif age>=13 and age<20 :
+    print('teen')
+elif age>=20 and age<50 :
+    print('adult')
+elif age>=50 :
+    print('old')
