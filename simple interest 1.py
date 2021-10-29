63a0647992baefa607338da486bc9276a55432a5
udayuk7 <93322281+udayuk7@users.noreply.github.com>
2021-10-29 16:17:44 +0530

    Add files via upload

---
+++ simple interest 1.py
@@ -0,0 +1,6 @@
+"Simple inerest"
+p = int(input("Enter the value of Principal="))
+t = int(input("Enter the time="))
+r = float(input("Enter the rate of inerest ="))
+S_I = p*t*r//100
+print("Simple Interest=", S_I)
