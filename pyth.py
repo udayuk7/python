29fae60c8ea20810116225ba0b0ced4486d83016
udayuk7 <93322281+udayuk7@users.noreply.github.com>
2021-10-29 16:15:54 +0530

    Add files via upload

---
+++ pyth.py
@@ -0,0 +1,7 @@
+n = int(input("Enter the value of n:"))
+s = n*(n+1)
+s1 = n**2
+s2 = n*(n+1)//2
+print("The sum of first", n, "even natural numbers=", s)
+print("The sum of first", n, "odd natural numbers=", s1)
+print("The sum of first", n, " natural numbers=", s2)
