fc4ce797b74795144ea39c5624b2b7204d33dcfc
udayuk7 <93322281+udayuk7@users.noreply.github.com>
2021-10-30 16:51:23 +0530

    Add files via upload

---
+++ if else elif.py
@@ -0,0 +1,28 @@
+#take a number from user and print month
+n=int(input('enter a number'))
+if n==1:
+    print('january')
+elif n==2:
+    print('february')
+elif n==3:
+    print('march')
+elif n==4:
+    print('april')
+elif n==5:
+    print('may')
+elif n==6:
+    print('june')
+elif n==7:
+    print('july')
+elif n==8:
+    print('august')
+elif n==9:
+    print('september')
+elif n==10:
+    print('october')
+elif n==11:
+    print('november')
+elif n==12:
+    print('december')
+else :
+    print('no month with this number')
