8ce7c6190f3958693c906fd7d27bea77485f9c51
udayuk7 <93322281+udayuk7@users.noreply.github.com>
2021-10-30 17:19:14 +0530

    Add files via upload

---
+++ else elif-2.py
@@ -0,0 +1,10 @@
+n=(int(input()))
+if n==1 or 3 or 5 or 7 or 8 or 10 or 12 :
+   print('month',n,'has 31 days')
+elif n==4 or 6 or 9 or 11 :
+    print('month',n,'has 30 days')
+elif n==2 :
+    print('month',n,'has 28 days')
+    
+    
+    
