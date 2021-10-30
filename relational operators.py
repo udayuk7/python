93d757111dd4c5c5f54556e4d94b20e0ae351863
udayuk7 <93322281+udayuk7@users.noreply.github.com>
2021-10-30 17:34:29 +0530

    Add files via upload

---
+++ relational operators.py
@@ -0,0 +1,6 @@
+#take a alphabet from user and find out wheather it is vowel or consonent
+a=input('enter a alphabet')
+if a== 'a' or a=='e' or a=='i' or a=='o' or a=='u':
+    print('it is a vowel')
+else :
+    print('it is a consonent')
